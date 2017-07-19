e8ba95cdec55c368da985f744f6a876ef98bd290
fairofox <fairo@hotmail.com>
2017-07-18 19:59:17 -0500

    base de datos, carrito

--- Comercio_PetMarket/PetMarket/Controllers/HomeController.cs
+++ Comercio_PetMarket/PetMarket/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace PetMarket.Controllers
         public ActionResult Buscar(String id="")
         {   
             //Logica para el acceso a la base de datos
-            var productos = bd.Producto.Where(x => x.Denominacion.Contains(id)).ToList();
+            var productos = bd.Producto.Where(x => x.Descripcion.Contains(id)).ToList();
             //Lista de productos
             ViewBag.clave = id;
             return View(productos);
