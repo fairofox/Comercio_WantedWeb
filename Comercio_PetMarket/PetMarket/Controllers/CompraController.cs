6f52dfba283da3971b21226b5a258910881f40e3
fairofox <fairo@hotmail.com>
2017-08-02 20:50:47 -0500

    checkout1

---
+++ Comercio_PetMarket/PetMarket/Controllers/CompraController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetMarket.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Paso1()
+        {
+            return View();
+        }
+        public ActionResult Paso2()
+        {
+            return View();
+        }
+        public ActionResult Paso3()
+        {
+            return View();
+        }
+        public ActionResult Paso4()
+        {
+            return View();
+        }
+    }
+}
